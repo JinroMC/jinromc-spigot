d89a8d8a92d08214b0e94fac21c3ecc6f78f4f2d
azurata <azurata@vivaldi.net>
2020-12-26 10:53:31 +0900

    Initial Commit

---
+++ src/main/java/me/azurata/jinromc/JinroMC.java
@@ -0,0 +1,17 @@
+package me.azurata.jinromc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class JinroMC extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
